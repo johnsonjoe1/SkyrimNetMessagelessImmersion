df5cdd2263ac77bc2be7d2e79437aafae2e888a4
Joe Johnson <johnsonjoe1@gmx.de>
2026-06-11 07:08:18 +0200

    Started reading all the global variables from SLSF-mod.  This works nicely.  Private message boxes implemented to understand the mod.

--- src/handle_yps.cpp
+++ src/handle_yps.cpp
@@ -18,6 +18,8 @@ struct ParsedCondition
     std::optional<int> value;
 };
 
+bool yps_high_heels_thought_flag_on = false;
+
 
 ParsedCondition ParseCondition(const std::string& str)
 {
@@ -57,13 +59,12 @@ std::unordered_set<std::string> ParseConditions(const std::string& str)
 			// Let's handle some special cases or specially interesting variables
 			auto c = ParseCondition(current_substring);
 
-			logger::info("Name = {}", c.name);
 
+			
+			logger::info("Name = {}", c.name);
 			if (c.value) {
 				logger::info("Value = {}", *c.value);
 			} else { logger::info("Value = missing / no value supplied"); }
-
-
 		}
 		start = end + 1;
 	}
@@ -95,58 +96,5 @@ void handle_yps::handle_yps_fashion_detection_stuff()
 	std::unordered_set<std::string> current_unorderd_yps_set = ParseConditions(current_yps_condition_string);
 
 
-
-	/*
-	auto* storage = StorageUtilAPI::GetSingleton();
-
-	int addictionLevel =
-		storage->GetIntValue(
-			nullptr,
-			"yps_AddictionLevel",
-			0);
-
-	int addictionBuff =
-		storage->GetIntValue(
-			nullptr,
-			"yps_AddictionBuff",
-			0);
-			
-	*/
-
-	// We have from another mod:
-	//
-	// GlobalVariable function GetINeedFatigue() global
-	// return Game.GetFormFromFile(0x12DC, "iNeed.esp") as GlobalVariable
-	// endFunction
-	//
-	// This should allow for direct native access to the same from C++:
-	//
-	// 
-	/* 
-	auto* fatigueGV =
-		RE::TESDataHandler::GetSingleton()
-			->LookupForm<RE::TESGlobal>(0x12DC, "iNeed.esp");
-	if (fatigueGV) {
-		float fatigue = fatigueGV->value;
-		logger::info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>iNeed Fatigue GlobalVariable found: value={}", fatigue);
-		if (fatigue == 0) {
-			logger::info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>iNeed Fatigue GlobalVariable found: i guess that means NO FATIGUE AT ALL, NOTHING! ");
-			if ( (fatigue < previous_iNeed_fatigue_level) & (previous_iNeed_fatigue_level != 1000000 ) ) {
-				DumpThoughts::throw_out_IMPORTANT_TTS_thought_message(std::format("You are full rested from sleep and you are completely rid of your fatigue now!  Say so in your response and let us know how that makes you feel!  And make it clear that you speak about your fatigue in your response!"));
-				SKSE::log::info("Note:  Fatigue-level-update thought 1 was delivered.");
-			}							
-		} else if (fatigue == 1) {
-			logger::info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>iNeed Fatigue GlobalVariable found: i guess that means MILD FATIGUE! ");
-			if (fatigue > previous_iNeed_fatigue_level) {
-				DumpThoughts::throw_out_IMPORTANT_TTS_thought_message(std::format("You are feeling mild fatigue.  Say so in your response and let us know how that makes you feel!  And make it clear that you speak about your fatigue in your response!"));
-				SKSE::log::info("Note:  Fatigue-level-update thought 2 was delivered.");
-			}
-		} else {
-			logger::info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>iNeed Fatigue GlobalVariable found: i guess that means some unknown level of fatigue! ");
-		}
-		// We only update fatigue, if the whole iNeed stuff worked.
-		previous_iNeed_fatigue_level = fatigue;					
-	}
-	*/
 }
 
--- src/plugin.cpp
+++ src/plugin.cpp
@@ -403,6 +403,167 @@ public:
 
 		handle_yps::handle_yps_fashion_detection_stuff();
 
+		// Some new stuff there:  Test what we can easily get from SLSF 3.4.1
+		// GlobalVariable Property SLSF_CurrentFamePCLocation_Group Auto Hidden
+		// GlobalVariable Property SLSF_CurrentFamePCLocation_Khajiit Auto Hidden
+
+		//auto* fameGroup = RE::TESForm::LookupByEditorID<RE::TESGlobal>("SLSF_CurrentFamePCLocation_Group");
+		// SLSF_Reloaded_CurrentExhibitionistFame
+		auto* fameGroup = RE::TESForm::LookupByEditorID<RE::TESGlobal>("SLSF_Reloaded_CurrentExhibitionistFame");
+
+		if (fameGroup) {
+			float value = fameGroup->value;
+			SKSE::log::info("NEW+NEW+NEW+NEW:   SLSF-Lookup worked out of the box! SLSF_Reloaded_CurrentExhibitionistFame:  {}", value);
+			
+		} else {
+			SKSE::log::info("NEW+NEW+NEW+NEW:   SLSF-Lookup DID NOT WORK STRAIGHT out of the box!");
+		}
+
+		struct FameGlobal
+		{
+			// index-number
+			// previous_value
+			// current_value
+			uint32_t formID;
+			const char* name;
+			RE::TESGlobal* global;
+			float previous_value;
+			float current_value;
+		};
+
+		static std::vector<FameGlobal> fameGlobals = {
+{ 0x0080C , "SLSF_Reloaded_CurrentSlutFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0080D , "SLSF_Reloaded_CurrentWhoreFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0080E , "SLSF_Reloaded_CurrentExhibitionistFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0080F , "SLSF_Reloaded_CurrentOralFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00810 , "SLSF_Reloaded_CurrentAnalFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00811 , "SLSF_Reloaded_CurrentNastyFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00812 , "SLSF_Reloaded_CurrentPregnantFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00813 , "SLSF_Reloaded_CurrentDominantFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00814 , "SLSF_Reloaded_CurrentSubmissiveFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00815 , "SLSF_Reloaded_CurrentSadistFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00816 , "SLSF_Reloaded_CurrentMasochistFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00817 , "SLSF_Reloaded_CurrentGentleFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00818 , "SLSF_Reloaded_CurrentMenFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00819 , "SLSF_Reloaded_CurrentWomenFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0081A , "SLSF_Reloaded_CurrentOrcFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0081B , "SLSF_Reloaded_CurrentKhajiitFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0081C , "SLSF_Reloaded_CurrentArgonianFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0081D , "SLSF_Reloaded_CurrentBestialityFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0081E , "SLSF_Reloaded_CurrentGroupFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0081F , "SLSF_Reloaded_CurrentBoundFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00820 , "SLSF_Reloaded_CurrentTattooFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00821 , "SLSF_Reloaded_CurrentCumDumpFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00822 , "SLSF_Reloaded_CurrentCheatFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00823 , "SLSF_Reloaded_CurrentCuckFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x0082F , "SLSF_Reloaded_CurrentAirheadFame" , nullptr, -99.0f, -99.0f } , 
+{ 0x00824 , "SLSF_Reloaded_OralCumVisible" , nullptr, -99.0f, -99.0f } , 
+{ 0x00825 , "SLSF_Reloaded_AnalCumVisible" , nullptr, -99.0f, -99.0f } , 
+{ 0x00826 , "SLSF_Reloaded_VaginalCumVisible" , nullptr, -99.0f, -99.0f } , 
+{ 0x0082A , "SLSF_Reloaded_CommentFrequency" , nullptr, -99.0f, -99.0f } , 
+{ 0x0082B , "SLSF_Reloaded_VisiblyBound" , nullptr, -99.0f, -99.0f } , 
+{ 0x0082C , "SLSF_Reloaded_LightlyBound" , nullptr, -99.0f, -99.0f } , 
+{ 0x0082D , "SLSF_Reloaded_HeavilyBound" , nullptr, -99.0f, -99.0f } , 
+{ 0x0082E , "SLSF_Reloaded_Skooma" , nullptr, -99.0f, -99.0f } , 
+{ 0x00830 , "SLSF_Reloaded_IsBelted" , nullptr, -99.0f, -99.0f } , 
+{ 0x00831 , "SLSF_Reloaded_IsCollared" , nullptr, -99.0f, -99.0f } , 
+{ 0x00835 , "SLSF_Reloaded_OralCumCount" , nullptr, -99.0f, -99.0f } , 
+{ 0x00836 , "SLSF_Reloaded_AnalCumCount" , nullptr, -99.0f, -99.0f } , 
+{ 0x00837 , "SLSF_Reloaded_VaginalCumCount" , nullptr, -99.0f, -99.0f } , 
+{ 0x00838 , "SLSF_Reloaded_TotalCumCount" , nullptr, -99.0f, -99.0f } , 
+{ 0x00829 , "SLSF_AllowComment" , nullptr, -99.0f, -99.0f } , 
+{ 0x0080B , "SLSF_Reloaded_NPCScanSucess" , nullptr, -99.0f, -99.0f } , 
+{ 0x00805 , "SLSF_Reloaded_CustomLocationCount" , nullptr, -99.0f, -99.0f } 
+		};
+
+		for (auto& fame : fameGlobals) {
+			if (!fame.global) {
+				fame.global = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESGlobal>( fame.formID, "SLSF Reloaded.esp");
+				SKSE::log::info("NEW+NEW+NEW+NEW:   WE HAD TO LOOK UP THE POINTER FOR SLSF- GLOBAL:  {}", fame.name);
+				// In this case (game probably), we assign the current value and previous value as well, so there is 'no change'
+				fame.previous_value = fame.global->value;
+				fame.current_value = fame.global->value;
+			} else {
+				SKSE::log::info("NEW+NEW+NEW+NEW:   SLSF-GLOBALPointer was already there for:  {}", fame.name);
+				float cur_value = fame.global->value;
+				SKSE::log::info("NEW+NEW+NEW+NEW:   SLSF-GLOBALPointer -> Value was :  {}", cur_value);
+				// Assign new value and message-box-report value changes for now
+				
+				fame.current_value = fame.global->value;
+				if (fame.current_value != fame.previous_value) {
+					if ( fame.current_value > fame.previous_value ) {
+						// Increase:  say as much
+						SKSE::log::info("SLSF-Handling: Detected and INCREASE in: {}  from {} to {}.", fame.name, fame.previous_value, fame.current_value);
+						LillithOnlyBox(std::format("SLSF-Handling: Detected and INCREASE in: {} from {} to {}." , fame.name , fame.previous_value , fame.current_value));
+					} else {
+						// Decrease:  say as much
+						SKSE::log::info("SLSF-Handling: Detected and INCREASE in: {}  from {} to {}.", fame.name, fame.previous_value, fame.current_value);
+						LillithOnlyBox(std::format("SLSF-Handling: Detected and DECREASE in: {} from {} to {}." , fame.name , fame.previous_value , fame.current_value));
+					}
+				}
+				fame.previous_value = fame.global->value;
+			}
+		}
+
+
+		auto* global =
+			RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESGlobal>(
+				0x805,
+				"SLSF Reloaded.esp");
+
+		if (global) {
+			float value_from_FormID = global->value;
+			SKSE::log::info("NEW+NEW+NEW+NEW:   SLSF-Lookup via FormID:  {}", value_from_FormID);
+		} else {
+			SKSE::log::info("NEW+NEW+NEW+NEW:   SLSF-Lookup via FormID DID NOT WORK STRAIGHT out of the box!");
+
+		}
+
+/*
+FE000805
+FE00080B
+FE00080C
+FE00080D
+FE00080E
+FE00080F
+FE000810
+FE000811
+FE000812
+FE000813
+FE000814
+FE000815
+FE000816
+FE000817
+FE000818
+FE000819
+FE00081A
+FE00081B
+FE00081C
+FE00081D
+FE00081E
+FE00081F
+FE000820
+FE000821
+FE000822
+FE000823
+FE000824
+FE000825
+FE000826
+FE000829
+FE00082A
+FE00082B
+FE00082C
+FE00082D
+FE00082E
+FE00082F
+FE000830
+FE000831
+FE000835
+FE000836
+FE000837
+FE000838
+*/
+
 
 		// We have from another mod:
 		// PlayerDirt = Game.GetFormFromFile(0x000DA8, "Bathing in Skyrim.esp") as GlobalVariable
