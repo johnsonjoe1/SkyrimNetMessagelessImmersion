b8e2a58fed260a03d196d74d575210e2def63813
Joe Johnson <johnsonjoe1@gmx.de>
2026-06-12 07:28:38 +0200

    Added potential fix for yps heels messages when no heels are worn.

--- src/handle_yps.cpp
+++ src/handle_yps.cpp
@@ -11,7 +11,7 @@
 #include <optional>
 
 namespace logger = SKSE::log;
-bool yps_high_heels_thought_flag_on = false;
+bool actually_wearing_heels_according_to_yps_thoughts = false;
 
 
 struct ParsedCondition
@@ -34,7 +34,7 @@ ParsedCondition ParseCondition(const std::string& str)
 }
 std::unordered_set<std::string> ParseConditions(const std::string& str)
 {
-	yps_high_heels_thought_flag_on=false;
+	actually_wearing_heels_according_to_yps_thoughts = false;
 	std::unordered_set<std::string> result;
 	size_t start = 0;
 	while (start < str.size()) {
@@ -62,7 +62,7 @@ std::unordered_set<std::string> ParseConditions(const std::string& str)
 			SKSE::log::info("YPS-String-Parsing: {} name: {} value: {}", current_substring, c.name, value_info);
 
 			if (c.name == "ypsHeelsWorn") {
-				yps_high_heels_thought_flag_on = true;
+				actually_wearing_heels_according_to_yps_thoughts = true;
 			}
 		}
 		start = end + 1;
@@ -84,17 +84,12 @@ void handle_yps::handle_yps_fashion_detection_stuff()
 		logger::info("SEVERE ERROR: Querying the player failed in the handle_yps_fashion_detection_stuff function!!");
 		return;
 	}
-
-
-
 	// Now, PERIODICALLY, we take care of changes in the values of other mods.  We put this here, because this gets
-	// triggered reasonably often.  
+	// triggered reasonably often.  Not clean, admittedly, but cleaning that up will have to come later.
 	std::string current_yps_condition_string = SNMIPapyrus::GetYpsConditionString();
 	logger::info("The lates YPS-condition-String was: {}" , current_yps_condition_string);
 	SKSE::log::info("Current yps_condition_string: {}", current_yps_condition_string);  
 	std::unordered_set<std::string> current_unorderd_yps_set = ParseConditions(current_yps_condition_string);
-
-
 }
 
 
@@ -109,30 +104,36 @@ void handle_yps::handle_yps_magic_effect_stuff(const RE::TESActiveEffectApplyRem
 	auto base_name = base->GetName();
 
 		// Let's try to track YPS-Fashion-High-Heels-slowdown-effect:  this gets activated and deactivated all the time, so we have to keep our messages in check
-		if (base && ( (std::strcmp(base->GetName(), "Movement Speed Penalty") == 0)  ) && ( (std::strcmp(source->GetName(), "High Heel Novice") == 0)  ) && (a_event->isApplied) && (effect->magnitude < 0) )
+		if (base && ( (std::strcmp(base->GetName(), "Movement Speed Penalty") == 0)  ) && ( (std::strcmp(source->GetName(), "High Heel Novice") == 0)  ) )
 		{
-			SKSE::log::info("xxxxxxxxxxx High Heels Novice MAGIC EFFECT PROPERLY DETECTED");
-			std::string stomach_rot_status = std::format("{} Magic Event Effect Handler for YPS HIGH HEELS NOVICE! ", base_name);
-			DumpThoughts::throw_out_BACKGROUND_TTS_thought_message(std::format("YOU, the player, are currently wearing high heels. You already have some experience with them, but you are still a High Heels Novice, so they still slow you down a bit. It will take maybe another day or two until you get the hang of them and can move a bit faster in them.  Say as much in your response.")); //  + standard_thought_instruction;);   // this shouldn't be overdone, but hte background code makes sure of that.
-			// return RE::BSEventNotifyControl::kContinue;
+			if ( (a_event->isApplied) )
+			{
+				// Due to what seems like a bug to me, there will be slowdown effect even when not wearing heels.  In that case of course we don't complain about the heels.  So we have to catch that here.
+				if ( (effect->magnitude < 0) && actually_wearing_heels_according_to_yps_thoughts) {
+					SKSE::log::info("xxxxxxxxxxx High Heels Novice MAGIC EFFECT PROPERLY DETECTED");
+					DumpThoughts::throw_out_BACKGROUND_TTS_thought_message(std::format("YOU, the player, are currently wearing high heels. You already have some experience with them, but you are still a High Heels Novice, so they still slow you down a bit. It will take maybe another day or two until you get the hang of them and can move a bit faster in them.  Say as much in your response.")); //  + standard_thought_instruction;);   // this shouldn't be overdone, but hte background code makes sure of that.
+				}
+				// NOTE:  Return-Control from Effect Handler will be done outside in the calling function!!!!   return RE::BSEventNotifyControl::kContinue;
+			} else {  //  i.e. if (!a_event->isApplied) {
+				// We do nothing here, as we just have stopped moving, nothing else.
+				SKSE::log::info("xxxxxxxxxxx SKIPPING:  IT's REMOVAL of High Heels Novice MAGIC EFFECT.");
+				// NOTE:  Return-Control from Effect Handler will be done outside in the calling function!!!!   return RE::BSEventNotifyControl::kContinue;
+			}
 		}
-		if (base && ( (std::strcmp(base->GetName(), "Movement Speed Penalty") == 0)  ) && ( (std::strcmp(source->GetName(), "High Heel Novice") == 0)  ) && (!a_event->isApplied) ) {
-			// We do nothing here.
-			SKSE::log::info("xxxxxxxxxxx SKIPPING:  IT's REMOVAL of High Heels Novice MAGIC EFFECT.");
-			// return RE::BSEventNotifyControl::kContinue;
-		}
-		if (base && ( (std::strcmp(base->GetName(), "Movement Speed Penalty") == 0)  ) && ( (std::strcmp(source->GetName(), "Untrained Feet") == 0)  ) && (a_event->isApplied) && (effect->magnitude < 0) )
+		if (base && ( (std::strcmp(base->GetName(), "Movement Speed Penalty") == 0)  ) && ( (std::strcmp(source->GetName(), "Untrained Feet") == 0)  ) ) 
 		{
-			SKSE::log::info("xxxxxxxxxxx YPS 'Untrained Feet' (high heels) MAGIC EFFECT PROPERLY DETECTED");
-			std::string stomach_rot_status = std::format("{} Magic Event Effect Handler for YPS UNTRAINED FEET! ", base_name);
-			DumpThoughts::throw_out_BACKGROUND_TTS_thought_message(std::format("YOU, the player, are currently wearing high heels. You are totally untrained with high heels. You are not even a High Heels Novice yet. So they slow you down massively now.  It will take maybe another day or two until you get the hang of them and can move a bit faster in them.  Say as much in your response.")); //  + standard_thought_instruction;);   // this shouldn't be overdone, but hte background code makes sure of that.
-			// return RE::BSEventNotifyControl::kContinue;
+			if ((a_event->isApplied) && (effect->magnitude < 0) )
+			{
+				SKSE::log::info("xxxxxxxxxxx YPS 'Untrained Feet' (high heels) MAGIC EFFECT PROPERLY DETECTED");
+				std::string stomach_rot_status = std::format("{} Magic Event Effect Handler for YPS UNTRAINED FEET! ", base_name);
+				DumpThoughts::throw_out_BACKGROUND_TTS_thought_message(std::format("YOU, the player, are currently wearing high heels. You are totally untrained with high heels. You are not even a High Heels Novice yet. So they slow you down massively now.  It will take maybe another day or two until you get the hang of them and can move a bit faster in them.  Say as much in your response.")); //  + standard_thought_instruction;);   // this shouldn't be overdone, but hte background code makes sure of that.
+				// NOTE:  Return-Control from Effect Handler will be done outside in the calling function!!!!   return RE::BSEventNotifyControl::kContinue;
+			}
+			if ( (!a_event->isApplied) ) {
+				// We do nothing here, as we just have stopped moving, nothing else.
+				SKSE::log::info("xxxxxxxxxxx SKIPPING:  IT's REMOVAL of Untrained Feet MAGIC EFFECT.");
+				// NOTE:  Return-Control from Effect Handler will be done outside in the calling function!!!!   return RE::BSEventNotifyControl::kContinue;
+			}
 		}
-		if (base && ( (std::strcmp(base->GetName(), "Movement Speed Penalty") == 0)  ) && ( (std::strcmp(source->GetName(), "Untrained Feet") == 0)  ) && (!a_event->isApplied) ) {
-			// We do nothing here.
-			SKSE::log::info("xxxxxxxxxxx SKIPPING:  IT's REMOVAL of Untrained Feet MAGIC EFFECT.");
-			// return RE::BSEventNotifyControl::kContinue;
-		}
-
 	logger::info("*********************YPS-MAGIC-EFFECT-HANDLER FINISHED!!!**********************************");
 }
