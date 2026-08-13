5b5c9f8d8c3ec388583fa23556c5902d89216254
Joe Johnson <johnsonjoe1@gmx.de>
2026-08-13 21:22:02 +0200

    YOU, the player, just tripped over your own feet, because you are wearing these devious bondage boots or leg cuffs that you are locked into and cannot take off

--- src/handle_yps.cpp
+++ src/handle_yps.cpp
@@ -99,7 +99,7 @@ bool handle_yps::try_handle_yps_mod_stuff(const SKSE::ModCallbackEvent* a_event)
 	if ( (std::strcmp(a_event->eventName.c_str() , "YPS_ThoughtEvent") == 0)  ) {
 		// std::string  thought_message = std::format(a_event->strArg.c_str());
 		DumpThoughts::throw_out_AS_LITTERAL_AS_POSSIBLE_thought_message(a_event->strArg.c_str());   // this shouldn't be overdone, but the background code makes sure of that.
-		return false;  // In this case it really was a YPS event and that means no further processing necessary in the main mod boadcast module.
+		return true;  // In this case it really was a YPS event and that means no further processing necessary in the main mod boadcast module.
 	}
 
 	if ( (std::strcmp(a_event->eventName.c_str() , "yps_AddictionBuffChange") == 0) ) {			
