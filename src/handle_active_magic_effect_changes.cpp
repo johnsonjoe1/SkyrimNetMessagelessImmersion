e091a758ccba109c0051496c41d45dc0310d851d
Joe Johnson <johnsonjoe1@gmx.de>
2026-08-17 13:38:02 +0200

    Added handling of the ChainSoundEffect from ankle chains.

--- src/handle_active_magic_effect_changes.cpp
+++ src/handle_active_magic_effect_changes.cpp
@@ -392,6 +392,29 @@ void handle_changes_in_active_magic_effects( const RE::TESActiveEffectApplyRemov
 		}
 		return;  // This will then be done in the calling function:   return RE::BSEventNotifyControl::kContinue;
 	}
+/*[2026-08-16 17:56:54.805] [log] [info] [handle_active_magic_effect_changes.cpp:535] ========== Found A SO-FAR UNHANDLED effect, that is actually about the Player.  Let's go into more details below! =============
+[2026-08-16 17:56:54.805] [log] [info] [handle_active_magic_effect_changes.cpp:536] Effect APPLIED on Lillith | UID=31
+[2026-08-16 17:56:54.805] [log] [info] [handle_active_magic_effect_changes.cpp:539] Base name: ChainSoundEffect | Base ptr: 0x20e3799c480 | Base-FormID: 1101BB51 | Base-Form Type: 18   (This means: MGEF) 
+[2026-08-16 17:56:54.805] [log] [info] [handle_active_magic_effect_changes.cpp:540] base-Effect EDID: zadx_HR_ChainSoundMagicEffect | Source ptr: 0x20e384a2740  |  Caster: Lillith 
+[2026-08-16 17:56:54.805] [log] [info] [handle_active_magic_effect_changes.cpp:544] Magnitude: 0 | Duration: 0
+[2026-08-16 17:56:54.805] [log] [info] [handle_active_magic_effect_changes.cpp:547] Source name: AnkleShackles-Enchant | Source FormID: 1102F518 | Source EDID: zadx_EnchAnkleShackles 
+[2026-08-16 17:56:54.805] [log] [info] [handle_active_magic_effect_changes.cpp:553] Form LookupByID 1101BB51 found: ChainSoundEffect*/
+	if (base && ( (std::strcmp(base_name, "ChainSoundEffect") == 0)  ) && ( (std::strcmp(source->GetName(), "AnkleShackles-Enchant") == 0)  ))
+	{
+		if (a_event->isApplied)
+		{
+			SKSE::log::info("Event handler for ChainSoundEffect APPLICATION!");
+			std::string final_thought_string = std::format("YOU, the player, are wearing ankle shackles with a chain, that is making sounds with every movement, so that it's impossible to move silently and sneak away in these things.  Say as much in your response, and be sure to make it clear, that you speak about the sounds from the ankle chains that you are wearing."); //  + standard_thought_instruction;
+			DumpThoughts::throw_out_TTS_thought_message("Active Effect: ChainSoundEffect: THOUGHT: " + final_thought_string);
+			LillithOnlyBox(final_thought_string);
+		} 
+		else // i.e.  if (!a_event->isApplied) )
+		{
+			// Do nothing here, since we only explain the effect when it is applied, that this is a special propery of the current corset.
+		}
+		return;  // This will then be done in the calling function:   return RE::BSEventNotifyControl::kContinue;
+	}
+
 
 
 /*[2026-08-16 17:56:42.354] [log] [info] [handle_active_magic_effect_changes.cpp:535] ========== Found A SO-FAR UNHANDLED effect, that is actually about the Player.  Let's go into more details below! =============
