8c874152f51b0c9ff763f5240f77b01811e019e2
Joe Johnson <johnsonjoe1@gmx.de>
2026-09-13 11:01:41 +0200

    Handled missing-pickaxe situations via the notification, because nothing else worked.

--- src/handle_debug_notifications.cpp
+++ src/handle_debug_notifications.cpp
@@ -11,6 +11,7 @@ namespace
 	REL::Relocation<ProcessMessage> original_process_message;
 	static auto last_gag_notification_thought_timestamp = std::chrono::steady_clock::now() - std::chrono::hours(1);
 	static auto last_surrender_notification_thought_timestamp = std::chrono::steady_clock::now() - std::chrono::hours(1);
+	static auto last_pickaxe_notification_thought_timestamp = std::chrono::steady_clock::now() - std::chrono::hours(1);
 
 	RE::UI_MESSAGE_RESULTS process_hud_message(RE::HUDMenu* a_menu, RE::UIMessage& a_message)
 	{
@@ -45,8 +46,7 @@ void check_for_relevant_notifications(const char* notification)
 		if (!cooldown_has_passed(last_gag_notification_thought_timestamp, 10)) {
 			return;
 		}
-
-		// RE::DebugMessageBox("Notification detected: You can't eat or drink while wearing this gag.!");
+		// LillithOnlyBox("Notification detected: You can't eat or drink while wearing this gag.!");
 		std::string  thought_message = std::format("YOU, the player, just tried to eat or drink something, but the gag you are wearing prevented it.  This is not only about thirst or hunger, but also about drinking potions for magic effect, so don't assume you know which item the attempt to consume was about.  The fact is, that you just tried hard, but you couldn't manage to get anything into your mouth or throat with that gag still locked onto you.  Say so in your response.");
 		DumpThoughts::throw_out_IMPORTANT_TTS_thought_message(thought_message);   // this should be rare enough to use the important TTS thought channel.
 		last_gag_notification_thought_timestamp = std::chrono::steady_clock::now();
@@ -58,7 +58,6 @@ void check_for_relevant_notifications(const char* notification)
 		if (!cooldown_has_passed(last_surrender_notification_thought_timestamp, 10)) {
 			return;
 		}
-
 		// RE::DebugMessageBox("Notification detected: You are surrendering!");
 		std::string  thought_message = std::format("YOU, the player, don't want to die and chose to just surrender to your enemies.  Say so in your response and let us know how you feel about it.");
 		DumpThoughts::throw_out_IMPORTANT_TTS_thought_message(thought_message);   // this should be rare enough to use the important TTS thought channel.
@@ -67,5 +66,16 @@ void check_for_relevant_notifications(const char* notification)
 		SKSE::log::info("Test failed.  This isnt: You are surrendering! Non-Relevant: {}", notification);
 	}
 	
+	if (strcmp(notification, "You need a pickaxe to use this.") == 0) {
+		if (!cooldown_has_passed(last_pickaxe_notification_thought_timestamp, 6)) {
+			return;
+		}
+		LillithOnlyBox("Notification detected: You need a pickaxe to use this.");
+		std::string  thought_message = std::format("YOU, the player, were trying to use a mining spot for mining ores, but you just found out, you don't have a pickaxe on you.  Without a pickaxe you can't mine anything, it's as simple as that.  Say so in your response and let us know how you feel about it.");
+		DumpThoughts::throw_out_IMPORTANT_TTS_thought_message(thought_message);   // this should be rare enough to use the important TTS thought channel.
+		last_pickaxe_notification_thought_timestamp = std::chrono::steady_clock::now();
+	} else {
+		SKSE::log::info("Test failed.  This isnt: You need a pickaxe to use this. Non-Relevant: {}", notification);
+	}	
 
 }
